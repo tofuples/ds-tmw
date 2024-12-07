699c13594e014ff6b1bb91eef89e2e06ddf30da6
Julia Elisa <hanksdaniels@gmail.com>
2024-12-07 02:20:58 -0300

    tabnews

--- data-collect-main/Tabnews/basic_content.py
+++ data-collect-main/Tabnews/basic_content.py
@@ -1,5 +1,9 @@
 # %% 
 import requests
+import pandas as pd
+import datetime
+import json
+import time
 # %%
 
 def get_response(**kwargs):
@@ -7,9 +11,47 @@ def get_response(**kwargs):
     resp = requests.get(url, params=kwargs)
     return resp
 
+def save_data(data, option='json'):
+    now = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S.%f")
+    if option == 'json':
+        with open(f'data/contents/json/{now}.json', 'w') as open_file:
+            json.dump(data, open_file, indent=4)
+    
+    elif option == 'parquet':
+        df = pd.DataFrame(data)
+        df.to_parquet(f"data/contents/parquet/{now}.parquet", index=False)
+    
+# %%
+
+page = 1
+date_stop = pd.to_datetime('2024-03-01').date()
+while True:
+    print(page)
+    resp = get_response(page=page, per_page=100, strategy="new")
+    if resp.status_code == 200:
+        data = resp.json()
+        save_data(data)
+        
+        date = pd.to_datetime(data[-1]["updated_at"])      
+        if len(data) < 100 or date < date_stop:
+            break
+        
+        page += 1  
+        time.sleep(5)
+        
+    else:
+        print(resp.status_code)
+        print(resp.json())
+        time.sleep(60 * 15)
+# %%
+
+data = resp.json()
+data
+# %%
+save_data(data)
 
 
 # %%
-resp = get_response(page=1, per_page=100, strategy="new")
-resp.json()
+date = "2024-03-19T17:36:17.376Z"
+pd.to_datetime(date).date()
 # %%

feaeff4e77656b52664ebbddc8ce33e1b7e1142f
Julia Elisa <hanksdaniels@gmail.com>
2024-12-09 23:18:19 -0300

    dados_re

--- data-collect-main/ResidentEvil/collect.py
+++ data-collect-main/ResidentEvil/collect.py
@@ -82,3 +82,12 @@ df = pd.DataFrame(data)
 df
 
 # %%
+df.to_csv("dados_re.csv", index=False, sep=";")
+# %%
+df.to_parquet("dados_re.parquet", index=False, )
+
+# %%
+
+df_new = pd.read_parquet("dados_re.parquet")
+df_new
+# %%
